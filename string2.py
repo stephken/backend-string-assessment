04003bfe170c2a22ebfdb48fa9f4df0d04b52dd1
ken stephens <kenstephens33@gmail.com>
2020-05-20 01:02:47 -0400

    prt2

--- string2.py
+++ string2.py
@@ -4,7 +4,7 @@ Kenzie assignment: String2
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Ken, Howard"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -23,8 +23,11 @@ __author__ = "???"
 
 
 def verbing(s):
-    # your code here
-    return
+    if len(s) < 3:
+        return s
+    elif s.endswith("ing"):
+        return s + "ly"
+    return s + "ing"
 
 
 # E. not_bad
@@ -37,8 +40,14 @@ def verbing(s):
 
 
 def not_bad(s):
-    # your code here
-    return
+    n = s.find("not", 0, len(s))
+    b = s.find("bad", 0, len(s))
+    if b > n:
+        if s[-1] in ("?", "!", "."):
+            return s[:n] + "good" + s[-1]
+        else:
+            return s[:n] + "good"
+    return s            
 
 
 # F. front_back
@@ -52,8 +61,21 @@ def not_bad(s):
 
 
 def front_back(a, b):
-    # your code here
-    return
+    if len(a) %2 == 0:
+        a_front = a[:len(a)//2]
+        a_back = a[len(a)//2:]
+    else:
+         a_front = a[:len(a)//2 + 1] 
+         a_back = a[len(a)//2 + 1:] 
+
+    if len(b) %2 == 0:
+        b_front = b[:len(b)//2]
+        b_back = b[len(b)//2:]
+    else:
+         b_front = b[:len(b)//2 + 1] 
+         b_back = b[len(b)//2 + 1:] 
+           
+    return a_front + b_front + a_back +b_back
 
 
 # Provided simple test() function used in main() to print
